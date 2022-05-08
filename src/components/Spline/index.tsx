cd402b0b5f053a1ba5a5fffbe2d335dc248b1bcd
Matheus Simões <matheusaugustosimoes@hotmail.com>
2022-05-07 21:45:42 -0300

    ajuste na animação

--- src/components/Spline/index.tsx
+++ src/components/Spline/index.tsx
@@ -1,13 +1,15 @@
 import dynamic from 'next/dynamic';
 
+import {Container} from "./styles"
+
 const Spline = dynamic(() => import('@splinetool/react-spline'), {
   ssr: false,
 });
 
 export default function App() {
   return (
-    <div>
+    <Container>
       <Spline scene="https://prod.spline.design/28DlSC-g9BrTlRgb/scene.splinecode" />
-    </div>
+    </Container>
   );
 }
---
+++ src/components/Spline/styles.ts
@@ -0,0 +1,12 @@
+import { lighten } from 'polished';
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-height: 50vh;
+
+`;
+
